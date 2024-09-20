d461ea9bd5bb48ef17729d1d2e29540079436aea
Mukil-boogeyman <mukilbikku1@gmail.com>
2024-09-20 16:52:17 +0530

    new update

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log("Team-1");
+console.log("i am mukil");
+import React from 'react';
+
+const hello = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default hello;
+
